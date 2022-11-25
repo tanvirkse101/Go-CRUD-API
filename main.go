af1a4d3480be4d983544ae662446f82b8293f724
tanvirkse101 <tanvirkse@gmail.com>
2022-11-25 20:50:57 +0600

    CRUD API

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"C/Git-Projects/Go-CRUD-API/controllers"
+	"C/Git-Projects/Go-CRUD-API/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{"data": "Test working"})
+	// })
+
+	models.ConnectDatabase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
